e7e50df23ead97287a1ded2444ccfb69c9783da7
Tomáš Jelínek <jelinekt.99@gmail.com>
2026-01-19 22:16:10 +0100

    init project

---
+++ src/consts.ts
@@ -0,0 +1,2 @@
+export const SITE_TITLE = 'Tomáš Jelínek';
+export const SITE_DESCRIPTION = 'Osobní portfólio a blog kde se dozvíš více o webu a aplikacích';
---
+++ src/content.config.ts
@@ -0,0 +1,19 @@
+import { defineCollection, z } from 'astro:content';
+import { glob } from 'astro/loaders';
+
+const blog = defineCollection({
+	// Load Markdown and MDX files in the `src/blog/` directory.
+	loader: glob({ base: './src/blog', pattern: '**/*.{md,mdx}' }),
+	// Type-check frontmatter using a schema
+	schema: ({ image }) =>
+		z.object({
+			title: z.string(),
+			description: z.string(),
+			// Transform string to Date object
+			pubDate: z.coerce.date(),
+			updatedDate: z.coerce.date().optional(),
+			heroImage: image().optional(),
+		}),
+});
+
+export const collections = { blog };
